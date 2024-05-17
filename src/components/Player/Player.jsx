6640828587552859ee77cd3829f5ad6e40ad8bce
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-05-17 21:53:24 +0300

    Create Player.jsx

---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,12 @@
+import ReactPlayer from 'react-player/youtube';
+import css from './Player.module.css';
+
+export default function Player({ trailer }) {
+  const ulrTrailer = `https://www.youtube.com/watch?v=${trailer}`;
+
+  return (
+    <div className={css.trailerContainer}>
+      <ReactPlayer url={ulrTrailer} width="800px" height="600px" />
+    </div>
+  );
+}
