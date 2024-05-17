b54e0d5adac817792c45bb7c94e843eafb919126
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-05-17 21:52:10 +0300

    Update App.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,33 @@
+import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('../../pages/MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('../../pages/MovieDetailsPage/MovieDetailsPage'));
+const MovieReviews = lazy(() => import('../MovieReviews/MovieReviews'));
+const MovieCast = lazy(() => import('../MovieCast/MovieCast'));
+
+import Layout from '../Layout/Layout';
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
+import Trailer from '../Trailer/Trailer';
+
+export default function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:filmId" element={<MovieDetailsPage />}>
+          <Route path="review" element={<MovieReviews />} />
+          <Route path="cast" element={<MovieCast />} />
+          <Route path="trailer" element={<Trailer />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Layout>
+  );
+}
+
+
+
+
