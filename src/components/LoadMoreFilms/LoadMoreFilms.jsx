59cfd39317b5abf51050f225936311af1942db00
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-05-17 21:52:39 +0300

    Create LoadMoreFilms.jsx

---
+++ src/components/LoadMoreFilms/LoadMoreFilms.jsx
@@ -0,0 +1,18 @@
+import css from './LoadMoreFilms.module.css';
+
+export default function LoadMoreFilms({ next, prev, totalPage, page }) {
+  return (
+    <div className={css.container}>
+      {page > 1 && (
+        <button className={css.btnMovies} onClick={prev}>
+          Prev page
+        </button>
+      )}
+      {totalPage > page && (
+        <button className={css.btnMovies} onClick={next}>
+          Next page
+        </button>
+      )}
+    </div>
+  );
+}
