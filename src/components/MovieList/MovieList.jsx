33f13cdd8226417ff235a4890df2fd08211a2a7a
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-05-17 21:53:04 +0300

    Create MovieList.jsx

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,31 @@
+import { Link, useLocation } from 'react-router-dom';
+import css from './MovieList.module.css';
+
+export default function MovieList({ films }) {
+  const location = useLocation();
+
+  return (
+    <ul className={css.filmContainer}>
+      {films.map(film => (
+        <li className={css.filmContainerItem} key={film.id}>
+          <Link
+            className={css.filmLink}
+            to={`/movies/${film.id}`}
+            state={location}
+          >
+            <img
+              className={css.filmImg}
+              src={
+                film.poster_path
+                  ? `https://image.tmdb.org/t/p/w200${film.poster_path}`
+                  : 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/640px-No-Image-Placeholder.svg.png'
+              }
+              alt={film.original_title}
+            />
+            <p className={css.filmTitle}>{film.original_title}</p>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
