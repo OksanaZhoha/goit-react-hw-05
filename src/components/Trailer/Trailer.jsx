eef41f8bd24c030a65cdebb73a612c25ba39a30c
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-05-17 21:53:31 +0300

    Create Trailer.jsx

---
+++ src/components/Trailer/Trailer.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { fetchTrailerFilms } from '../../movie-trailer-api';
+import Player from '../Player/Player';
+import Loading from '../../components/Loading/Loading';
+import Error from '../../components/Error/Error';
+import { useParams } from 'react-router-dom';
+
+export default function Trailer() {
+  const [trailer, setTrailer] = useState('');
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { filmId } = useParams();
+
+  useEffect(() => {
+    const trailerFilm = async () => {
+      try {
+        setError(false);
+        setLoading(true);
+        const data = await fetchTrailerFilms(filmId);
+        setTrailer(data.results[0].key);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    trailerFilm();
+  }, [filmId]);
+
+  return (
+    <div>
+      {loading && <Loading />}
+      {error && <Error />}
+      <Player trailer={trailer} />
+    </div>
+  );
+}
